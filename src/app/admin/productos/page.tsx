3e716227e9baa39ac0110a6715ec18fed9c26570
LocalhostDiluk <sebas.lozano.reyes@gmail.com>
2025-09-11 14:39:50 -0600

    updated

--- src/app/admin/productos/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/admin/products/page.tsx  (Server Component)
-import { getAllProducts } from "@/lib/data/products";
-import ProductsClient from "./products-client";
-
-export default async function ProductsPage() {
-  const products = await getAllProducts();
-
-  return <ProductsClient products={products} />;
-}
